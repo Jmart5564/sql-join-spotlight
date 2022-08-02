7bd18c865568a7411e9b8fd3067b2699a3384090
Jmart5564 <Jmart5564@gmail.com>
2022-08-02 15:05:46 -0700

    12 passing

--- exercise_11/exercise_11.sql
+++ exercise_11/exercise_11.sql
@@ -3,9 +3,9 @@
 -- GROUP BY the film_id
 -- ORDER in descending order
 -- LIMIT 10
-SELECT film.title, count(rental) FROM film
+SELECT title, COUNT(rental) FROM film
 INNER JOIN inventory ON inventory.film_id = film.film_id
 INNER JOIN rental ON rental.inventory_id = inventory.inventory_id
 GROUP BY film.film_id
-ORDER BY count DESC
+ORDER BY COUNT DESC
 LIMIT 10;
